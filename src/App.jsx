0c1b709a8ad327365dc101397bf03a4063614882
Coco0818 <nikeyouxiang@qq.com>
2020-11-23 14:10:01 +0800

    1

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,12 @@ import React, { Component } from 'react'
 // 引入个人设置组件
 import Settings from '../src/pages/user/center/Settings'
 
-
 export default class App extends Component {
   render() {
-    return <div>
-    <Settings />
-    </div>
+    return (
+      <div>
+        <Settings />
+      </div>
+    )
   }
 }
